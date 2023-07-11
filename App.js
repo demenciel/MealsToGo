9db6746577b8e3664b690c706294650a7e275c6c
alexcouture <alexcouture5@hotmail.com>
2023-07-10 20:11:34 -0400

    do

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, TextInput, Platform } from 'react-native';
 
 export default function App() {
+  const isAndroid = Platform.OS === "android";
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.input}>
+          <TextInput>This is a react native text canvas</TextInput>
+        </View>
+        <View style={styles.restaurantList}>
+          <Text>This is a react native text canvas</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  input: {
+    backgroundColor: 'green',
+    padding: 16,
     justifyContent: 'center',
   },
-});
+  restaurantList: {
+    backgroundColor: 'red',
+    flex: 1,
+    padding: 16,
+    justifyContent: 'flex-start',
+  }
+}); 
